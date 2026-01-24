6f2231c660acec252a8976fca0dd69ea99415ceb
nguyenvanluan1 <nguyenvanluan2005vg@gmail.com>
2026-01-24 20:39:47 +0700

    Ví dụ đầu tiên
    Để demo

---
+++ HelloWorld/src/luan/vn/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package luan.vn;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Toi ten la Nguyen Van Luan");
+	}
+
+}
